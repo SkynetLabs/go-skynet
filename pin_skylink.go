53db42baeb4f643a56c70f0dd62d57257ec5ca12
jay-dee7 <jasdeepsingh.uppal@gmail.com>
2021-12-06 00:11:29 +0530

    Feat: Added Pin Support for Skylinks
    Signed-off-by: jay-dee7 <jasdeepsingh.uppal@gmail.com>

---
+++ pin_skylink.go
@@ -0,0 +1,45 @@
+package skynet
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"gitlab.com/NebulousLabs/errors"
+)
+
+var (
+	PinEndpoint      = "/skynet/pin/"
+	SkylinkHeaderKey = "skynet-skylink"
+)
+
+func (sc *SkynetClient) PinSkylink(skylink string) (string, error) {
+	skylink = strings.TrimPrefix(skylink, "sia://")
+
+	resp, err := sc.executeRequest(
+		requestOptions{
+			reqBody: nil,
+			query:   url.Values{},
+			Options: Options{
+				EndpointPath:      PinEndpoint,
+				APIKey:            sc.Options.APIKey,
+				CustomUserAgent:   sc.Options.CustomUserAgent,
+				CustomCookie:      sc.Options.CustomCookie,
+				customContentType: sc.Options.customContentType,
+			},
+			method:    http.MethodPost,
+			extraPath: skylink,
+		},
+	)
+	if err != nil {
+		return "", errors.AddContext(err, "could not execute request")
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+		return "", fmt.Errorf("expected response status code to be %d but got %d", http.StatusNoContent, resp.StatusCode)
+	}
+
+	pinLink := resp.Header.Get(SkylinkHeaderKey)
+	return pinLink, nil
+}
